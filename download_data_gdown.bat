5a1f55a6bdeb25f0d5196920f862e59f8e9eb803
LcChinh21 <leducchinhdz@gmail.com>
2026-05-23 07:05:30 +0000

    commit

--- download_data_gdown.bat
+++ download_data_gdown.bat
@@ -12,7 +12,19 @@ REM Cai dat gdown
 python -m pip install --upgrade pip
 python -m pip install gdown
 
-REM Tai Google Drive folder
+REM Tai Google Drive folder data
 gdown --folder 1gT_Iy4S7ZmpiF4PWhPYWOsHK-f9Ysk-y
+
+cd ..
+
+REM Di chuyen vao thu muc outputs, neu chua co thi tao moi
+if not exist "outputs\" (
+    mkdir outputs
+)
+
+cd outputs
+
+REM Tai Google Drive folder outputs
+gdown --folder 1Q6hgEHTZS73n2NzmUJ031wGU1EqssN55
 
 pause
